be55aeef2cec2cfc72797a51dfea77b8cae63ef8
amirsaeid93 <said.taebnia@gmail.com>
2025-10-06 18:11:34 +0300

    Update

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ export DISPLAY=:99
 # Start the VNC server, allowing connections from anywhere without a password
 x11vnc -display :99 -nopw -forever &
 
-# Run the Java application using a classpath
-# This tells Java to look for classes in our app.jar and in all the JARs inside the libs/ folder.
-java -cp "app.jar:libs/*" application.Main
+# Run the Java application using a classpath AND the module path.
+# --module-path libs: Tells Java to look for the JavaFX modules inside the libs/ folder.
+# -cp "app.jar:libs/*": Tells Java to find our application code and all other libraries.
+java --module-path libs --add-modules javafx.controls,javafx.fxml -cp "app.jar:libs/*" application.Main
