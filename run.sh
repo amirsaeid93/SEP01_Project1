0ccb1f9832ea2f9b76c7018c1f4bc4aef9513ea9
amirsaeid93 <said.taebnia@gmail.com>
2025-10-06 21:59:50 +0300

    Update

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-set -e
+#set -e
 
-echo "Starting virtual display (Xvfb)..."
-Xvfb :99 -screen 0 1280x1024x24 &
+#echo "Starting virtual display (Xvfb)..."
+#Xvfb :99 -screen 0 1280x1024x24 &
 
 # Wait briefly to ensure Xvfb is fully started
-sleep 2
+#sleep 2
 
-echo "Starting VNC server..."
-x11vnc -display :99 -nopw -forever -shared -rfbport 5900 &
+#echo "Starting VNC server..."
+#x11vnc -display :99 -nopw -forever -shared -rfbport 5900 &
 
-echo "Launching JavaFX application..."
+#echo "Launching JavaFX application..."
 # Use software rendering and specify module path for JavaFX
-java \
-  -Dprism.order=sw \
-  -Djava.awt.headless=false \
-  --module-path libs \
-  --add-modules javafx.controls,javafx.fxml \
-  -cp "app.jar:libs/*" \
-  application.Main
+#java \
+ # -Dprism.order=sw \
+ # -Djava.awt.headless=false \
+ # --module-path libs \
+ # --add-modules javafx.controls,javafx.fxml \
+ # -cp "app.jar:libs/*" \
+ # application.Main
